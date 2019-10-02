50f8eaa8bd92e70b6695b903087cbea0012ee0c5
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-10-02 10:57:01 -0300

    Prepara headers da requisição

--- cbo.py
+++ cbo.py
@@ -20,3 +20,21 @@ def get_dtpinfra_token(resp):
         resp.content.decode('latin-1')
     )
     return dtpinfra_token.group(1)
+
+
+def prepare_headers():
+    headers = {
+        'Connection': 'keep-alive',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
+        'Origin': 'http://www.mtecbo.gov.br',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'Referer': 'http://www.mtecbo.gov.br/cbosite/pages/pesquisas/BuscaPorCodigo.jsf',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+    }
+
+    return headers
