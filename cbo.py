43c63ce59ff4cfdb7254bc03dbcc764ceaaeb8b1
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-10-02 10:56:35 -0300

    Corrige regex para javax faces viewstate

--- cbo.py
+++ cbo.py
@@ -8,7 +8,7 @@ URL = 'http://www.mtecbo.gov.br/cbosite/pages/pesquisas/BuscaPorCodigo.jsf'
 
 def get_javax_faces_viewstate(resp):
     view_state = re.search(
-        r'id="javax.faces.ViewState" value="(-?\d+:\d+)"',
+        r'id="javax.faces.ViewState" value="(-?\d+:-?\d+)"',
         resp.content.decode('latin-1')
     )
     return view_state.group(1)
